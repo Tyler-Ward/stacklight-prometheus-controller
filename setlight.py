63cd856d962a9bd45a459cfd8a01d3ab91092dfc
Tyler Ward <tyler@scorpia.co.uk>
2023-03-07 21:24:37 +0000

    Add a hysterisis function
    This adds the option to set a lower threshold to avoid the light
    flickering

--- setlight.py
+++ setlight.py
@@ -10,7 +10,9 @@ prometheus_address = os.environ.get("PROMETHEUS_ADDRESS","localhost")
 prometheus_port = os.environ.get("PROMETHEUS_PORT","9090")
 prometheus_query = os.environ.get("PROMETHEUS_QUERY")
 threshold_yellow = int(os.environ.get("THRESHOLD_YELLOW",50))
+threshold_yellow_down = int(os.environ.get("THRESHOLD_YELLOW_DOWN",threshold_yellow))
 threshold_red = int(os.environ.get("THRESHOLD_RED",75))
+threshold_red_down = int(os.environ.get("THRESHOLD_RED_DOWN",threshold_red))
 stacklight_address = os.environ.get("STACKLIGHT_ADDRESS")
 
 if prometheus_query is None:
@@ -25,6 +27,7 @@ headers = {
         'X-HTTP-Method-Override': 'GET'
         }
 
+colour = "off"
 
 while True:
     r = requests.get(request_url,
@@ -37,16 +40,36 @@ while True:
         value = int(data["data"]["result"][0]["value"][1])
         # print(value)
 
-        colour="off"
-        if value >= threshold_yellow:
-            colour="yellow"
-        if value >= threshold_red:
-            colour="red"
+        if colour == "off":
+            if value >= threshold_yellow:
+                colour_next="yellow"
+            if value >= threshold_red:
+                colour_next="red"
+        elif colour == "yellow":
+            if value < threshold_yellow_down:
+                colour_next="off"
+            if value >= threshold_red:
+                colour_next="red"
+        elif colour == "red":
+            if value < threshold_red_down:
+                colour_next="yellow"
+            if value < threshold_yellow_down:
+                colour_next="off"
+        else:
+            colour = "off"
+            colour_next = "off"
+            
+        if colour_next != colour:
+            colour=colour_next
+            print("setting to {}".format(colour))
 
-        print("setting to {}".format(colour))
-        r = requests.post(
-            "http://{}/set".format(stacklight_address),
-            data="mode={}".format(colour),
-            verify=False)
+        try:
+            r = requests.post(
+                "http://{}/set".format(stacklight_address),
+                data="mode={}".format(colour),
+                verify=False)
+        except requests.exceptions.ConnectionError:
+            print("Unable to Connect to stacklight")
+
 
     time.sleep(5)
